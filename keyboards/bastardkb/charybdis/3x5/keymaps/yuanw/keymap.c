1715c13c718e4b03e77d877f0f6bec81c28e81c1
yuanw <me@yuanwang.ca>
2022-11-20 08:27:48 -0600

    format

--- keyboards/bastardkb/charybdis/3x5/keymaps/yuanw/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/yuanw/keymap.c
@@ -102,7 +102,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define LAYOUT_LAYER_POINTER                                                                  \
     XXXXXXX, XXXXXXX, XXXXXXX, DPI_MOD, S_D_MOD, S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, \
     ______________HOME_ROW_GACS_L______________, ______________HOME_ROW_GACS_R______________, \
-    _______, DRGSCRL, SNIPING, EEP_RST, QK_BOOT, QK_BOOT, EEP_RST, SNIPING, DRGSCRL, _______, \
+    _______, DRGSCRL, SNIPING, EEP_RST, QK_BOOT, QK_BOOT,EEP_RST, SNIPING, DRGSCRL, _______, \
                       KC_BTN2, KC_BTN1, KC_BTN3, KC_BTN3, KC_BTN1
 
 /**
